64f620334ea9f489436651cb39e91c26ea66cf22
Henrique <matosh38@gmail.com>
2024-10-05 09:59:14 -0300

    atualizado

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 
 carteira ={}
 opc = ['Criar usuário','Operações entre contas', 'Sair']
-operacoes = ['transferir', 'receber','ver saldo','excluir usuario','sair']
+operacoes = ['transferir', 'receber','excluir usuario','sair']
 
 # limpar o terminal
 def limpar():
@@ -33,7 +33,7 @@ def mostraruser():
 
 
 def transf(nome1, nome2):
-    if nome2 in carteira and nome1 in carteira:
+    if nome2.capitalize() in carteira and nome1.capitalize() in carteira:
         print(f'O seu saldo disponivel: R${carteira[nome1]['saldo']:.2f}')
         transferir = float(input("O quanto você quer transferir\n>>> ")) 
         if carteira[nome1]['saldo']>=transferir:
@@ -47,110 +47,95 @@ def transf(nome1, nome2):
         else:
             print(f"{nome} não tem saldo suficiente para realizar essa transferencia!")
             input()                            
-    elif nome2 not in carteira or nome1 not in carteira:
+    elif nome2.capitalize() not in carteira or nome1.capitalize() not in carteira:
         print(f"Usuário {nome1}/{nome2} inexistente!")
         input()
-    elif not nome2 and not nome1:
-        print("Rapaz")
+    elif not nome2 or not nome1:
+        print("Nenhum usuário foi encontrado.\nPressione enter para continuar...",end="")
         input()
-        return
     
-
+#tenta abrir o arquivo .json, caso não exista ou esteja vazia, chama a função para criar o novo usuário
 try:
     with open('./carteira.json', 'r') as file:
         files = file.read()
         carteira = json.loads(files)
-
 except:
     limpar()
     if len(carteira)==0:
-        criaruser()    
+        criaruser()
+ 
 while True:
     limpar()
     # mostra o primeiro menu de opções
     for i, k in enumerate(opc):
             print(f"{i+1} - {k}")
-    try:
-        esc= int(input(">>> "))
-        limpar()
-        match esc:
-            #Criar um novo usuário
-            case 1:
-                mostraruser()
-                criaruser()
+    esc= int(input(">>> "))
+    limpar()
+    match esc:
+        #Criar um novo usuário
+        case 1:
+            mostraruser()
+            criaruser()
 
-            # Mostra o menu com as operações
-            case 2:
-                for i, k in enumerate(operacoes):
-                    print(f"{i+1} - {k.capitalize()}")
-                chc = int(input(">>> "))
-                limpar()
-                match chc:
-                    #Transferir para outro usuário, mostra os usuários disponivel na tela, e em uma linha pede de quem para quem será transferido o valor caso tenha o saldo disponivel será realizada a transferencia se não voltará para a tela inicial
-                    case 1:
-                        while True:
-                            mostraruser()
-                            print("Quem vai traferir para quem:")
-                            print("Deixe a caixa em branco para sair da operação: ")
-                            try:
-                                nome1, nome2 = input(">>> ").split(' ')
-                            except ValueError as err:
-                                print(err)
-                                break
-                            transf(nome1, nome2)
-                            break
-                    # Um usuário irá receber o valor de outro usuário, pedindo o nome de quem vai receber de quem irá tirar o valor do primeiro e inserir na conta do segundo
-                    case 2:
-                        mostraruser()
-                        print("Quem vai receber de quem: ")
+        # Mostra o menu com as operações
+        case 2:
+            for i, k in enumerate(operacoes):
+                print(f"{i+1} - {k.capitalize()}")
+            chc = int(input(">>> "))
+            limpar()
+            mostraruser()
+            match chc:
+                #Transferir para outro usuário, mostra os usuários disponivel na tela, e em uma linha pede de quem para quem será transferido o valor caso tenha o saldo disponivel será realizada a transferencia se não voltará para a tela inicial
+                case 1:
+                    while True:
+                        print("Quem vai traferir para quem:")
                         print("Deixe a caixa em branco para sair da operação: ")
                         try:
                             nome1, nome2 = input(">>> ").split(' ')
                         except ValueError as err:
                             print(err)
-                            break   
-                        transf(nome2, nome1)
-                        break
-                    case 3:
-                        mostraruser()
-                        print('De quem você deseja ver o saldo: ')
-                        print("Deixe a caixa em branco para sair da operação")
-                        nome = str(input(">>> "))
-                        if nome in carteira:
-                            print(f"|{nome:^20}|")
-                            print(f"Saldo: R${carteira[nome]['saldo']:,.2f}")
-                            input()
-                        elif nome not in carteira:
-                            print(f"Usuário {nome} inexistente")
-                            input()
-                        elif not nome:
                             break
-                    case 4:
-                        mostraruser()
-                        print("Qual usuário você deseja excuir:")
-                        print("Deixe a caixa em branco para sair da operação")
-                        nome = str(input(">>> ")) 
-                        if nome in carteira:
-                            print("Digite confirmar para realizar essa ação:")
-                            conf = str(input(">>> ")).strip().lower()
-                            if conf in 'confirmar':
-                                del(carteira[nome])
-                                print(f"Usuário {nome} foi excluido com sucesso")
-                                input()
-                            elif conf not in 'confirmar' or not conf:
-                                print(f'Usuário: {nome} não foi excluido')
-                                input()
-                                break
-                    case 5: 
-                        break
-            case 3:
-                break
-    except ValueError:
-        print('valor inválido')
-        input()
+                        transf(nome1, nome2)
+                        input()
+
+                # Um usuário irá receber o valor de outro usuário, pedindo o nome de quem vai receber de quem irá tirar o valor do primeiro e inserir na conta do segundo
+                case 2:
+                    print("Quem vai receber de quem: ")
+                    print("Deixe a caixa em branco para sair da operação: ")
+                    try:
+                        nome1, nome2 = input(">>> ").split(' ')
+                    except ValueError as err:
+                        print(err)
+                        break   
+                    transf(nome2, nome1)
+                    input()
+
+                case 3:
+                    print("Qual usuário você deseja excuir:")
+                    print("Deixe a caixa em branco para sair da operação")
+                    nome = str(input(">>> ")) 
+                    if nome in carteira:
+                        print("Digite confirmar para realizar essa ação:")
+                        conf = str(input(">>> ")).strip()
+                        if conf in 'confirmar':
+                            del(carteira[nome])
+                            print(f"Usuário {nome} foi excluido com sucesso")
+                            input()
+                        elif conf not in 'confirmar' or not conf:
+                            print(f'Usuário: {nome} não foi excluido')
+                            input()
+                            break
+                case 4: 
+                    break
+                case _:
+                    print("Valor informador inválido, tente outro")
+        case 3:
+            break
+        case _:
+            print("Valor informador inválido, tente outro")
 try:
     # escreve em um arquivo json para quando o programa for aberto novamente os dados estejam lá
     with open('Carteira.json', 'w') as file:
-        file.write(json.dumps(carteira))
+        file.write(json.dumps(carteira, indent=4))
 except IndexError:
-    print('finalizando programa')
+    print('finalizando programa') 
